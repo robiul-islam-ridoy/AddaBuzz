7dd0cc9fa0870c4fb7d19fa8a788694dfd9efe4a
Robiul Islam <robiulislamhr.bd@gmail.com>
2025-06-23 00:24:40 +0600

    Login page UI created

--- app/src/main/java/com/example/addabuzz/login.java
+++ app/src/main/java/com/example/addabuzz/login.java
@@ -1,6 +1,5 @@
 package com.example.addabuzz;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class MainActivity extends AppCompatActivity {
-    FirebaseAuth auth;
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        auth = FirebaseAuth.getInstance();
-
-        if (auth.getCurrentUser() == null){
-            Intent intent = new Intent(MainActivity.this, login.class);
-            startActivity(intent);
-        }
     }
 }
